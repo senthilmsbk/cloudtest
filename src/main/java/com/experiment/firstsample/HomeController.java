85e000ac34535723f9385f0fab23f2c0048b0a8f
senthil <senthilmsbk@gmail.com>
2026-09-18 20:03:28 +0530

    testing

--- src/main/java/com/experiment/firstsample/HomeController.java
+++ src/main/java/com/experiment/firstsample/HomeController.java
@@ -3,11 +3,12 @@ package com.experiment.firstsample;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/home1")
     public String homePage(Model model) {
         model.addAttribute("name", "Lee"); // dynamic value
         return "home"; // loads home.html
